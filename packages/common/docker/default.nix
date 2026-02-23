93eece1e0887e9636f0874b10ec16aa0dd56d71b
Fred Clausen <43556888+fredclausen@users.noreply.github.com>
2026-02-23 08:53:42 -0700

    chore: set up auto pruning and stop open whatever being dumb

--- packages/common/docker/default.nix
+++ packages/common/docker/default.nix
@@ -1,19 +1,34 @@
-{ pkgs, ... }:
+{
+  pkgs,
+  ...
+}:
 {
   config = {
-    virtualisation.docker.enable = true;
+    virtualisation.docker = {
+      enable = true;
 
-    systemd.services.docker-create-adsbnet = {
-      description = "Create Docker network adsbnet";
-      after = [ "docker.service" ];
-      requires = [ "docker.service" ];
-
-      serviceConfig = {
-        Type = "oneshot";
-        ExecStart = ''${pkgs.runtimeShell} -c "${pkgs.docker}/bin/docker network create adsbnet || true"'';
+      autoPrune = {
+        enable = true;
+        dates = "daily";
+        flags = [ "--all" ];
       };
+    };
 
-      wantedBy = [ "multi-user.target" ];
+    systemd = {
+      services = {
+        docker-create-adsbnet = {
+          description = "Create Docker network adsbnet";
+          after = [ "docker.service" ];
+          requires = [ "docker.service" ];
+
+          serviceConfig = {
+            Type = "oneshot";
+            ExecStart = ''${pkgs.runtimeShell} -c "${pkgs.docker}/bin/docker network create adsbnet || true"'';
+          };
+
+          wantedBy = [ "multi-user.target" ];
+        };
+      };
     };
   };
 }
