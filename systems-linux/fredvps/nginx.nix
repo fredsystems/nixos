3aa16a8c0f4bf1fb128671596957ae51b2bf1408
Fred Clausen <43556888+fredclausen@users.noreply.github.com>
2026-02-24 14:41:54 -0700

    shut up the scanner

--- systems-linux/fredvps/nginx.nix
+++ systems-linux/fredvps/nginx.nix
@@ -29,7 +29,7 @@
           };
 
           "/cider-v3.1.8-linux-x64.AppImage" = {
-            alias = "/home/fred/cider-v3.1.8-linux-x64.AppImage";
+            root = "/home/fred";
             # extraConfig = ''
             #   add_header Content-Type application/octet-stream;
             #   add_header Content-Disposition "attachment; filename=cider-v3.1.8-linux-x64.AppImage";
